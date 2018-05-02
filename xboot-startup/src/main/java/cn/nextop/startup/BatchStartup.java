ee7d2680aace867d791166153018de311c8472f6
qutl <qutl>
2018-05-02 14:30:31 +0800

    z

--- xboot-startup/src/main/java/cn/nextop/startup/BatchStartup.java
+++ xboot-startup/src/main/java/cn/nextop/startup/BatchStartup.java
@@ -4,21 +4,18 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import cn.nextop.core.common.orm.service.UserService;
 
 @SpringBootApplication
 @MapperScan("cn.nextop.core.common.orm.dao")
 public class BatchStartup {
-	
-	@Autowired UserService service;
-	
 	//
 	private static final String[] CONFIGS = new String[] {"classpath:core-spring-orm.xml"};
 	
+	/**
+	 * Main 
+	 */
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(BatchStartup.class);
 		app.setSources(new HashSet<>(Arrays.asList(CONFIGS))); app.run(args);
